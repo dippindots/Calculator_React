ef3c2d757caf9fafe054265505bca53778795aa7
Gaofei Zhao <zgfilx@163.com>
2018-08-06 14:53:32 -0500

    UI basic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Button from "./Button";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -8,9 +9,39 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">React Calculator</h1>
         </header>
-        <p className="App-intro">
-          Template initialization.
-        </p>
+        <div>
+          <div className="component-display">
+            <div>0</div>
+          </div>
+          <div className="component-button-panel">
+            <div>
+              <Button name="C" wide />
+              <Button name="÷" orange />
+            </div>
+            <div>
+              <Button name="7" />
+              <Button name="8" />
+              <Button name="9" />
+              <Button name="x" orange />
+            </div>
+            <div>
+              <Button name="4" />
+              <Button name="5" />
+              <Button name="6" />
+              <Button name="-" orange />
+            </div>
+            <div>
+              <Button name="1" />
+              <Button name="2" />
+              <Button name="3" />
+              <Button name="+" orange />
+            </div>
+            <div>
+              <Button name="0" wide />
+              <Button name="=" orange />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/Button.js
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Button.css";
+
+class Button extends React.Component {
+  render() {
+    const className = [
+      "component-button",
+      this.props.orange ? "orange" : "",
+      this.props.wide ? "wide" : ""
+    ];
+
+    return (
+      <div className={className.join(" ").trim()}>
+        <button onClick={this.handleClick}>{this.props.name}</button>
+      </div>
+    );
+  }
+}
+export default Button;
